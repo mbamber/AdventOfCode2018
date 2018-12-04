7cc98cac6f0ab483b7fff5e95037b23c678dd593
Matthew Bamber <matthewbamber@icloud.com>
2018-12-04 18:51:03 +0000

    :heavy_check_mark: Solution for day02 part02

--- day02/main.py
+++ day02/main.py
@@ -1,42 +1,36 @@
 #! /usr/bin
 # -*- coding: UTF-8 -*-
 
+import sys
+
 def main():
 
     # Open and read the file
     with open('input', 'rb') as in_file:
         file_contents = in_file.readlines()
 
-    curr_totals = (0, 0)
-    for word in file_contents:
-        totals = two_and_three_count(word)
-        curr_totals = tuple(map(sum, zip(curr_totals, totals)))
+    for i in range(0, len(file_contents)):
+        first_word = file_contents[i]
+        for j in range(i+1, len(file_contents)):
+            second_word = file_contents[j]
 
-    checksum = curr_totals[0] * curr_totals[1]
-    print('The checksum is {checksum}'.format(checksum=checksum))
+            for x in range(1, 5):
+                if get_word_without_char(first_word, x) == get_word_without_char(second_word, x):
+                    print('The two words are {first_word} and {second_word}'.format(
+                        first_word=first_word,
+                        second_word=second_word
+                    ))
+                    print('The common part is {common_word} and the differing position is {pos}'.format(
+                        common_word=get_word_without_char(first_word, x),
+                        pos=x
+                    ))
+                    sys.exit(0)
 
-
-# Work out the number of times 2 letters and 3 letters appear in a word
-# Return two values of either 1 or 0 depending on if there are 2 letters or 3
-def two_and_three_count(word):
-    counts = letter_count(word)
-
-    has_two_letters = len(filter(lambda k: counts[k] == 2, counts)) > 0
-    has_three_letters = len(filter(lambda k: counts[k] == 3, counts)) > 0
-
-    return (1 if has_two_letters else 0, 1 if has_three_letters else 0)
-
-# Create a map of each letter to the number of times that it occurs
-def letter_count(word):
-    counts = {}
-
-    for letter in word:
-        if letter in counts:
-            counts[letter] += 1
-        else:
-            counts[letter] = 1
-
-    return counts
+# Remove the given character position from the specified word
+def get_word_without_char(word, char_pos):
+    first_part = word[:char_pos-1]
+    second_part = word[char_pos:]
+    return first_part + second_part
 
 if __name__ == '__main__':
     main()
