3300ede68b9e86323d0e314046418200c5936908
Matthew Bamber <matthewbamber@icloud.com>
2018-12-08 12:38:16 +0000

    :heavy_check_mark: Solution for day05 part01

---
+++ day05/main.py
@@ -0,0 +1,64 @@
+#! /usr/bin
+# -*- coding: UTF-8 -*-
+
+def main():
+
+    # Load in the polymer
+    with open('input', 'rb') as in_file:
+        file_contents = in_file.readlines()
+
+    polymer = file_contents[0].rstrip()
+
+    # Keep looping through the polymer until we didnt make a change
+    did_make_change_in_pass = True
+    while did_make_change_in_pass:
+
+        did_make_change_in_pass = False
+        new_polymer = ''
+
+        # Loop through the polymer, performing all the reactions
+        i = 0
+        while i < (len(polymer) - 1):
+            reaction_result = attempt_reaction(polymer[i], polymer[i+1])
+            new_polymer = new_polymer + reaction_result
+
+            if reaction_result == '':
+                did_make_change_in_pass = True
+                i += 1 # We need to skip the next unit, because it just reacted and dissapeared
+
+            i += 1
+
+        # Need to add the last unit in the polymer if it didnt react with the
+        # second-last unit, or the last attempted reaction succeded, but it was
+        # on the third last and second last units (i.e. the last unit is untouched)
+        if (not reaction_result == '') or (reaction_result == '' and (i < len(polymer))):
+            new_polymer = new_polymer + polymer[len(polymer) - 1]
+
+        polymer = new_polymer
+
+    print('The new polymer is {new_polymer} which has {num_units} units'.format(
+        new_polymer=polymer,
+        num_units=len(polymer)
+    ))
+
+
+# See if two units react, and either return an empty string (because they anihilated themselves)
+# or the first unit because there was no reaction
+def attempt_reaction(x, y):
+
+    # Check if the cases match
+    if x.isupper() and y.isupper():
+        return x
+
+    if x.islower() and y.islower():
+        return x
+
+    # Given that the cases do not match, check if they are the same letter
+    if x.upper() == y.upper():
+        return ''
+    else:
+        return x
+
+
+if __name__ == '__main__':
+    main()
