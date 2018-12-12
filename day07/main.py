99b686ec842d4a07faf08fcc37404f1133fac8bd
Matthew Bamber <matthewbamber@icloud.com>
2018-12-12 08:04:18 +0000

    :heavy_check_mark: Solution for day07 part02

--- day07/main.py
+++ day07/main.py
@@ -4,6 +4,8 @@
 import re
 
 LETTER_LIST = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+NUM_WORKERS = 5
+TASK_DEFAULT_TIME = 60
 
 def main():
 
@@ -25,15 +27,67 @@ def main():
 
     direct_decendants = reverse_map(direct_decendants)
 
-    ordered_instructions = ''
+    # Create a mapping of a finish time to tasks that are in progress
+    in_progress_tasks = {}
+    # Maintain a mapping of workers to the task they are currently working on
+    available_workers = {}
+    for i in range(0, NUM_WORKERS):
+        available_workers[i + 1] = None
 
-    while len(direct_decendants) > 0:
-        next_instruction, direct_decendants = get_next_instruction(direct_decendants)
-        ordered_instructions += next_instruction
+    t = 0
+    is_done = False
+    while not is_done:
+        # Stop any tasks that have now finished
+        new_finished_tasks = []
+        if t in in_progress_tasks:
+            new_finished_tasks = in_progress_tasks.pop(t)
 
-    print('The final order of instructions is: {final_order}'.format(
-        final_order=ordered_instructions
+        # Update the list of task letters that are in progress
+        task_letters_in_progress = []
+        for ipt_time in in_progress_tasks:
+            task_letters_in_progress += in_progress_tasks[ipt_time]
+        task_letters_in_progress = list(set(task_letters_in_progress))
+
+        # Update the list of workers that are now free
+        for worker in available_workers:
+            curr_task = available_workers[worker]
+            if curr_task in new_finished_tasks:
+                available_workers[worker] = None
+
+        # Update the dependency mapping to remove the tasks that are now complete
+        for finished_task in new_finished_tasks:
+            direct_decendants = remove_instruction_from_map(finished_task, direct_decendants)
+
+        # Update the list of tasks that can be started
+        tasks_ready_to_start = get_instructions_without_dependencies(direct_decendants)
+        tasks_ready_to_start = filter(lambda x: x not in task_letters_in_progress, tasks_ready_to_start)
+
+        # Start any new tasks
+        for worker in available_workers:
+            if available_workers[worker] == None:
+                if len(tasks_ready_to_start) > 0:
+                    task_to_start = tasks_ready_to_start.pop()
+                    task_finish_time = task_completion_time(task_to_start) + t
+
+                    # Assign the worker to the task
+                    available_workers[worker] = task_to_start
+                    # Add the task to the list of in_progress_tasks
+                    if task_finish_time in in_progress_tasks:
+                        in_progress_tasks[task_finish_time].append(task_to_start)
+                    else:
+                        in_progress_tasks[task_finish_time] = [task_to_start]
+
+        # Decide if we have completed all the tasks
+        if len(direct_decendants) == 0 and len(in_progress_tasks) == 0:
+            is_done = True
+            continue
+
+        t += 1
+
+    print('Tasks completed after {secs} seconds'.format(
+        secs=t
     ))
+
 
 def reverse_map(initial_map):
     new_map = {}
@@ -45,15 +99,6 @@ def reverse_map(initial_map):
             new_map[dependent].append(letter)
 
     return new_map
-
-def get_next_instruction(direct_decendants):
-    instructions_without_dependencies = get_instructions_without_dependencies(direct_decendants)
-    sorted_instructions = sorted(instructions_without_dependencies)
-    instruction = sorted_instructions[0]
-
-    new_dependencies = remove_instruction_from_map(instruction, direct_decendants)
-
-    return (instruction, new_dependencies)
 
 def remove_instruction_from_map(instruction, direct_decendants):
     new_dependencies = {}
@@ -76,6 +121,9 @@ def remove_instruction_from_map(instruction, direct_decendants):
 def get_instructions_without_dependencies(direct_decendants):
     instructions = filter(lambda instruction: len(direct_decendants[instruction]) == 0, direct_decendants)
     return instructions
+
+def task_completion_time(task):
+    return ord(task) - 64 + TASK_DEFAULT_TIME
 
 if __name__ == '__main__':
     main()
